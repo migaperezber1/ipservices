b1e081155acbaa2d841f1c6d530a4267dbd9b013
migaperezber1 <migaperezber@hotmail.com>
2018-02-22 10:25:28 -0500

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -2,17 +2,28 @@
 # -*- coding: utf-8 -*-
 import socket
 import sys
+import cv2
+import numpy as np
+import struct
+import pickle
+
+cap = cv2.VideoCapture(0)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(("192.168.0.29", 4444))
+s.connect(("localhost", 4444))
 
 while True:
-   mensaje = raw_input("> ")
-   s.send(mensaje)
-   recibido = s.recv(1024)
-   print (recibido)
-   if mensaje == "quit":
-     break
-
-print "adios"
+   
+   #mensaje = raw_input("> ")
+   #s.send(mensaje)
+   #recibido = s.recv(1024)
+   #print (recibido)
+   #if mensaje == "quit":
+    # break
+#print "adios" 
+    ret,frame = cap.read()
+    data = pickle.dumps(frame)#serializacion del frame, prepararlo para el envio.
+    s.sendall(struct.pack("L", len(data)) + data) #provee estructura de C para enviarlo en forma de bytes, provee los valores de tamado del dato, y el dato.
+    
+s.close()
--- servidor.py
+++ servidor.py
@@ -1,20 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import socket
+import sys
+import cv2
+import pickle
+import numpy as np
+import struct
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("192.168.0.29",4444))
-s.listen(5)
+s.bind(("localhost",4444))
+s.listen(1)
 
 sc, addr = s.accept()
+print "conexion aceptada"
+data = ""
+payload_size = struct.calcsize("L") #devuelve el tamaño de la cadena en ese formato.
+
 while True:
-
-     recibido = sc.recv(1024)
-     if recibido == "quit":
-       break
-     print ("Recibido:", recibido)
+    while len(data)< payload_size:
+        data += sc.recv(4096)
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("L", packed_msg_size)[0]
     
-     mensaje = raw_input("> ")
-     sc.send(mensaje)
+    while len(data) < msg_size:
+        data += sc.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
 
-print("chao")
+    frame = pickle.loads(frame_data)
+    print frame.size
+
+
+#     recibido = sc.recv(1024)
+ #    if recibido == "quit":
+  #     break
+   #  print ("Recibido:", recibido)
+    
+    # mensaje = raw_input("> ")
+     #sc.send(mensaje)
+
+#print("chao")
 
 sc.close()
 
