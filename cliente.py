e3cb4b389fc2ad208a6ec1ae6fd67bf74f2aaca3
migaperezber1 <migaperezber@hotmail.com>
2018-02-22 10:26:18 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -15,13 +15,13 @@ s.connect(("localhost", 4444))
 
 while True:
    
-   #mensaje = raw_input("> ")
+   """mensaje = raw_input("> ")
    #s.send(mensaje)
    #recibido = s.recv(1024)
    #print (recibido)
    #if mensaje == "quit":
     # break
-#print "adios" 
+#print "adios" """
     ret,frame = cap.read()
     data = pickle.dumps(frame)#serializacion del frame, prepararlo para el envio.
     s.sendall(struct.pack("L", len(data)) + data) #provee estructura de C para enviarlo en forma de bytes, provee los valores de tamado del dato, y el dato.
